e2637169e29f489d582d4b6afccb20c239fdc2c8
Bonamente <56506035@protonmail.com>
2023-02-22 14:12:27 +0500

    feat(chapter_5/5.12): add task solutions

---
+++ chapter_5/5.12/task_2/index.js
@@ -0,0 +1,21 @@
+// Напишите функцию replacer для JSON-преобразования, которая удалит свойства, ссылающиеся на meetup:
+let room = {
+  number: 23,
+};
+
+let meetup = {
+  title: 'Совещание',
+  occupiedBy: [{ name: 'Иванов' }, { name: 'Петров' }],
+  place: room,
+};
+
+// цикличные ссылки
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+alert(
+  JSON.stringify(meetup, function replacer(key, value) {
+    /* ваш код */
+    return key != '' && value == meetup ? undefined : value;
+  })
+);
